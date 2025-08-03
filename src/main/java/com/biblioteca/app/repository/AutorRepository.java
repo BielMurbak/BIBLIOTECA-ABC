de790058d0f5c7edf3a4a3c9cc6e8479bee7756e
Rafael Carlos Scarabelot <rafaelcarlosscarabelot@gmail.com>
2025-08-03 20:28:58 -0300

    terminando autor

--- src/main/java/com/biblioteca/app/repository/AutorRepository.java
+++ src/main/java/com/biblioteca/app/repository/AutorRepository.java
@@ -17,5 +17,36 @@ public class AutorRepository {
         autores.add(autor);
     }
 
+    public List<Autor> listAll() {
+        return autores;
+    }
+
+    public Autor findById(Integer id) {
+        for (Autor autor : autores) {
+            if (autor.getId() == id) {
+                return autor;
+            }
+        }
+        return null;
+    }
+
+    public void deleteById(Integer id) {
+        Autor autor = findById(id);
+        if (autor != null) {
+            autores.remove(autor);
+        }
+    }
+    public Autor update(Integer id, Autor autorUpdate) {
+        for (Autor autor : autores) {
+            if (autor.getId()==id) {
+                if (autorUpdate.getNome() != null) autor.setNome(autorUpdate.getNome());
+                if (autorUpdate.getCpf() != null) autor.setCpf(autorUpdate.getCpf());
+                if (autorUpdate.getIdade() != null) autor.setIdade(autorUpdate.getIdade());
+                return autor;
+            }
+        }
+        return null;
+    }
 
 }
+
